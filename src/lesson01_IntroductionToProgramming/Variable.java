85f7f90fb43a55cec0baf69a9dcd841e647acd87
Farid <farid@Farids-Mac-mini.local>
2023-12-08 15:40:48 +0400

    Java

---
+++ src/lesson01_IntroductionToProgramming/Variable.java
@@ -0,0 +1,16 @@
+package lesson01_IntroductionToProgramming;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        // Variable!
+        // DataType: DataName: Assignment: Value:
+        int         i          =       123;
+        System.out.println(i);
+
+        int number = 5;
+        number = 5+3;
+        System.out.println(number);
+
+    }
+}
